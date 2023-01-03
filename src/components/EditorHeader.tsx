6e7739f847c1031440849cfb1a10f88e7f527dba
Madusha Prasanjith <hi@madusha.me>
2023-01-03 14:59:13 +0100

    feat: Copy post URL to clipboard.

--- src/components/EditorHeader.tsx
+++ src/components/EditorHeader.tsx
@@ -1,6 +1,17 @@
+import { useRouter } from "next/router";
 import React, { type FC } from "react";
+import { toast } from "react-toastify";
+import useCopyToClipboard from "../hooks/useCopyToClipboard";
 
 const EditorHeader: FC<any> = ({ title, setTitle, onUpdate }) => {
+  const router = useRouter();
+  const [_, copyLink] = useCopyToClipboard();
+
+  function onCopy() {
+    copyLink(`https://foundryeditor.vercel.app/posts/${router.query.id}`);
+    toast.success("Copied to clipboard");
+  }
+
   return (
     <header className="flex items-center justify-between bg-gray-300 px-4 py-3">
       <input
@@ -10,8 +21,11 @@ const EditorHeader: FC<any> = ({ title, setTitle, onUpdate }) => {
         onChange={(e) => setTitle(e.target.value)}
       />
       <div className="flex gap-2">
-        <button className="rounded-lg bg-gray-500 py-2 px-4 font-bold text-white hover:bg-gray-700">
-          Share
+        <button
+          onClick={onCopy}
+          className="rounded-lg bg-gray-500 py-2 px-4 font-bold text-white hover:bg-gray-700"
+        >
+          Copy Link
         </button>
         <button
           onClick={onUpdate}
